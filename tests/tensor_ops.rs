78dcdbc81e80ce8b451b48f2de0693e927f7bc7a
caelunshun <caelum@vanispelen.me>
2024-09-28 16:08:13 -0600

    Add more test cases for basic tensor ops

--- tests/tensor_ops.rs
+++ tests/tensor_ops.rs
@@ -13,3 +13,52 @@ fn add() {
 
     assert_ulps_eq!(result.as_slice(), &[3.0f32; 100][..]);
 }
+
+#[test]
+fn multiply() {
+    let a = Tensor::from_vec(vec![2.0f32; 100], [50, 2], DEVICE);
+    let b = Tensor::from_vec(vec![bf16::from_f32(3.0); 100], [50, 2], DEVICE);
+
+    let result = (a * b).into_vec::<f32>();
+
+    assert_ulps_eq!(result.as_slice(), &[6.0f32; 100][..]);
+}
+
+#[test]
+fn divide() {
+    let a = Tensor::from_vec(vec![6.0f32; 100], [50, 2], DEVICE);
+    let b = Tensor::from_vec(vec![bf16::from_f32(2.0); 100], [50, 2], DEVICE);
+
+    let result = (a / b).into_vec::<f32>();
+
+    assert_ulps_eq!(result.as_slice(), &[3.0f32; 100][..]);
+}
+
+#[test]
+fn multiply_by_scalar() {
+    let a = Tensor::from_vec(vec![2.0f32; 100], [50, 2], DEVICE);
+    let scalar = 3.0f32;
+
+    let result = (a * scalar).into_vec::<f32>();
+
+    assert_ulps_eq!(result.as_slice(), &[6.0f32; 100][..]);
+}
+
+#[test]
+fn divide_by_scalar() {
+    let a = Tensor::from_vec(vec![6.0f32; 100], [50, 2], DEVICE);
+    let scalar = 2.0f32;
+
+    let result = (a / scalar).into_vec::<f32>();
+
+    assert_ulps_eq!(result.as_slice(), &[3.0f32; 100][..]);
+}
+
+#[test]
+fn recip() {
+    let a = Tensor::from_vec(vec![2.0f32; 100], [50, 2], DEVICE);
+
+    let result = a.recip().into_vec::<f32>();
+
+    assert_ulps_eq!(result.as_slice(), &[0.5f32; 100][..]);
+}
